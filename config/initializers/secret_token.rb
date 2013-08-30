e58c993e95b17390b68ada4147b3c0b0e13fc7ac
Ryan Huck <huck.j.ryan@gmail.com>
2013-08-29 20:12:48 -0400

    Created a rails applicationa dna dded status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'c5ce089391b741e915f2739323af9ce0908828198b2da6021d5bb32ebfa600ed6ee071769ec9e0394e14feda74e5514140b6853c3e2c7233f3602926382c5f1a'
